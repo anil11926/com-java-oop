51718780e8df1e7d21400226897f48d0441d1f8d
anil11926 <anilkumar955321336@gmail.com>
2026-04-17 15:26:23 +0530

    feat: Constructor added in both constumor and product

--- Customer.java
+++ Customer.java
@@ -3,9 +3,38 @@ package com.java.oop;
 public class Customer {
     int id;
     String userName;
-    String eMail;
+    String email;
     long contactNumber;
     byte age;
     char gender;
     String address;
+
+    // arg constructor
+    public Customer(int id, String userName, String email, long contactNumber, byte age, char gender, String address) {
+        System.out.println("Constructor called");
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.contactNumber = contactNumber;
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+    }
+// No-arg constructor
+    public Customer() {
+        System.out.println("Constructor called");
+        id = 123;
+        userName = "Balu Aadiila";
+        email = "balu@g,mail.com";
+        contactNumber = 348789515;
+        age = 22;
+        gender = 'M';
+        address = ";hyderabad";
+// not arg constructor
+
+
+
+
+
+    }
 }
--- Product.java
+++ Product.java
@@ -9,5 +9,35 @@ public class Product {
     byte discountPercentage;
     boolean isAvailable;
     String company;
-    String category;
+    String category ;
+
+    public Product(int id, String name, long maxRetailPrice, float rating, int reviewsCount, byte discountPercentage, boolean isAvailable, String company, String category) {
+        System.out.println("Constructor called");
+        this.id = id;
+        this.name = name;
+        this.maxRetailPrice = maxRetailPrice;
+        this.rating = rating;
+        this.reviewsCount = reviewsCount;
+        this.discountPercentage = discountPercentage;
+        this.isAvailable = isAvailable;
+        this.company = company;
+        this.category = category;
+    }
+
+    public Product() {
+        System.out.println("Constructor called");
+        id = 456;
+        name = "OnePlus 15 5G (Sand Storm, 256 GB) (12 GB RAM)";
+        maxRetailPrice = 76990;
+        rating = 4.7F;
+        reviewsCount = 135;
+        discountPercentage = 15;
+        isAvailable = true;
+        company = "one plus";
+        category = "Mobile";
+
+
+
+
+    }
 }
