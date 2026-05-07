13f956a72d610b06d4acba63205338d40531a45b
anil11926 <anilkumar955321336@gmail.com>
2026-05-07 22:59:39 +0530

    feat: Abstraction

--- abstraction/Main.java
+++ abstraction/Main.java
@@ -1,22 +1,21 @@
 package com.java.oop.abstraction;
 
-import com.java.oop.Customer;
-
 public class Main {
     static void main(String[] args) {
 
-        Customer customer=new Customer(1,"Customer1","customer1@gamil.com","451278963254",4578963214L);
-        ApplicationForm applicationForm = new ApplicationForm(1,"birth Certificate",customer);
+        Customer customer = new Customer(1,"vinay","vinay@gmail.com","451236987896",7896541230L);
+        ApplicationForm applicationForm = new ApplicationForm(1,"bunny",customer);
         Mro mro = new Mro(1,"MRO1",10000);
         Attender attender = new Attender(1,10000,"Attender",mro);
         PrintService printService = new ColourPrintService();
 
         CertificateService certificateService = new CertificateServiceImpl(attender);
         Certificate certificate = certificateService.getCertificate(applicationForm);
+        printService.printCertificate(certificate);
         System.out.println("Id : " + certificate.id
-             +   "\n Certificate Name : " + certificate.name
-        + "\n Certificate Issued by : " + certificate.issuedBy
-        + "\n Certificate Issued to : " + certificate.issuedTo
+                +   "\n Certificate Name : " + certificate.name
+                + "\n Certificate Issued by : " + certificate.issuedBy
+                + "\n Certificate Issued to : " + certificate.issuedTo
                 +"\n Certificate Issued date : " + certificate.issuedDate );
         printService.printCertificate(certificate);
     }
